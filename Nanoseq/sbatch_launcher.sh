89eb312851be2ad2a4321d851e0e6694bac0a539
l-matthias <matthias.lts@gmail.com>
2020-09-25 17:11:17 +0200

    Add parameters in sbatch_launcher.sh

--- Nanoseq/sbatch_launcher.sh
+++ Nanoseq/sbatch_launcher.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
-#SBATCH --job-name=double_nanoseq
 #SBATCH --chdir=${working_directory}
 #SBATCH --nodelist=genogpu2
 #SBATCH --gres=gpu:1 -p gpu
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=${CPU}
 #SBATCH --mem=30G
 
 working_directory=$(pwd)
 data_repo='/home/genouest/cnrs_umr6290/mlorthiois/nanoseq_test/data'
+CPU=16
 
 . /local/env/envnextflow-20.04.1.sh
 
 nextflow run nf-core/nanoseq \
     -r 1.0.0 \
     -profile singularity \
-    -with-singularity /groups/dog/matthias/singularity/nfcore-nanoseq-1.0.0.img \
-    --input ${working_directory}/input.csv \
+    -with-singularity /groups/dog/matthias/singularity/nfcore-nanoseq-dev.img \
+    --input ${working_directory}/input_first_step.csv \
     --protocol cDNA \
     --input_path ${data_repo} \
     --flowcell FLO-MIN106 \
     --kit SQK-DCS109 \
     --skip_demultiplexing \
-    --skip_qc \
-    --max_cpus 16
+    --max_cpus $CPU \
     --guppy_gpu true \
     --guppy_config /groups/dog/script/ont/ont-guppy/data/dna_r9.4.1_450bps_hac.cfg
 
 nextflow run nf-core/nanoseq \
-    --input ${working_directory}/input_fasta.csv \
+    --input ${working_directory}/input_second_step.csv \
     --protocol cDNA \
     --input_path ${data_repo} \
     --flowcell FLO-MIN106 \
     --kit SQK-DCS109 \
     --skip_basecalling \
     --skip_demultiplexing \
-    --max_cpus 16 \
+    --max_cpus $CPU \
     -profile singularity \
-    -with-singularity /groups/dog/matthias/singularity/nfcore-nanoseq-dev.img \
     -r 1.0.0
+
